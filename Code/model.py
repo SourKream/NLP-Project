b64fe891700a8f7eaffc925f4aac245d3cd01b04
Shantanu Kumar <kumarshantanu95@gmail.com>
2016-04-14 16:34:11 +0530

    Fixed Minor Bug

--- Code/model.py
+++ Code/model.py
@@ -23,12 +23,14 @@ def get_params():
     parser.add_argument('-lstm', action="store", default=150, dest="lstm_units", type=int)
     parser.add_argument('-epochs', action="store", default=20, dest="epochs", type=int)
     parser.add_argument('-batch', action="store", default=32, dest="batch_size", type=int)
-    parser.add_argument('-xmaxlen', action="store", default=20, dest="xmaxlen", type=int)
+    parser.add_argument('-xmaxlen', action="store", default=30, dest="xmaxlen", type=int)
     parser.add_argument('-ymaxlen', action="store", default=20, dest="ymaxlen", type=int)
     parser.add_argument('-nopad', action="store", default=False, dest="no_padding", type=bool)
     parser.add_argument('-lr', action="store", default=0.001, dest="lr", type=float)
     parser.add_argument('-load', action="store", default=False, dest="load_save", type=bool)
     parser.add_argument('-verbose', action="store", default=False, dest="verbose", type=bool)
+    parser.add_argument('-l2', action="store", default=0.0003, dest="l2", type=float)
+    parser.add_argument('-dropout', action="store", default=0.1, dest="dropout", type=float)
     opts = parser.parse_args(sys.argv[1:])
     print "lstm_units", opts.lstm_units
     print "epochs", opts.epochs
@@ -36,6 +38,8 @@ def get_params():
     print "xmaxlen", opts.xmaxlen
     print "ymaxlen", opts.ymaxlen
     print "no_padding", opts.no_padding
+    print "regularization factor", opts.l2
+    print "dropout", opts.dropout
     return opts
 
 def get_H_n(X):
@@ -75,37 +79,39 @@ def build_model(opts, verbose=False):
 #    InitWeights = np.load('VocabMat.npy')
     model.add_node(Embedding(InitWeights.shape[0], InitWeights.shape[1], input_length=N, weights=[InitWeights]), name='emb',
                    input='input')
-    model.add_node(Dropout(0.1), name='d_emb', input='emb')
+    model.add_node(Dropout(opts.dropout), name='d_emb', input='emb')
 
     model.add_node(LSTM(opts.lstm_units, return_sequences=True), name='forward', input='d_emb')
     model.add_node(LSTM(opts.lstm_units, return_sequences=True, go_backwards=True), name='backward', input='d_emb')
-    model.add_node(Dropout(0.1), name='dropout', inputs=['forward','backward'])
+    model.add_node(Dropout(opts.dropout), name='dropout', inputs=['forward','backward'])
 
     model.add_node(Lambda(get_H_n, output_shape=(k,)), name='h_n', input='dropout')
     model.add_node(Lambda(get_Y, output_shape=(L, k)), name='Y', input='dropout')
-    model.add_node(TimeDistributedDense(k,W_regularizer=l2(0.01)), name='WY', input='Y')
-
+    model.add_node(TimeDistributedDense(k,W_regularizer=l2(opts.l2)), name='WY', input='Y')
     ###########
     # "dropout" layer contains all h vectors from h_1 to h_N
 
     model.add_node(Lambda(get_H_hypo, output_shape=(N-L, k)), name='h_hypo', input='dropout')
-    model.add_node(TimeDistributedDense(k,W_regularizer=l2(0.01)), name='Wh_hypo', input='h_hypo')
+    model.add_node(TimeDistributedDense(k,W_regularizer=l2(opts.l2)), name='Wh_hypo', input='h_hypo')
 
     # GET R1
     f = get_WH_Lpi(0)
     model.add_node(Lambda(f, output_shape=(k,)), name='Wh_lp1', input='Wh_hypo')
     model.add_node(RepeatVector(L), name='Wh_lp1_cross_e', input='Wh_lp1')
     model.add_node(Activation('tanh'), name='M1', inputs=['Wh_lp1_cross_e', 'WY'], merge_mode='sum')
-    model.add_node(TimeDistributedDense(1,activation='softmax'), name='alpha1', input='M1')
+
+    Distributed_Dense_init_weight = ((2.0/np.sqrt(k)) * np.random.rand(k,1)) - (1.0 / np.sqrt(k))
+    Distributed_Dense_init_bias = ((2.0) * np.random.rand(1,)) - (1.0)
+    model.add_node(TimeDistributedDense(1,activation='softmax',weights=[Distributed_Dense_init_weight,Distributed_Dense_init_bias]), name='alpha1', input='M1')
     model.add_node(Lambda(get_R, output_shape=(k,1)), name='_r1', inputs=['Y','alpha1'], merge_mode='join')
     model.add_node(Reshape((k,)),name='r1', input='_r1')
 
     Tan_Wr_init_weight = 2*(1/np.sqrt(k))*np.random.rand(k,k) - (1/np.sqrt(k))
     Tan_Wr_init_bias = 2*(1/np.sqrt(k))*np.random.rand(k,) - (1/np.sqrt(k))
-    model.add_node(Dense(k,W_regularizer=l2(0.01),activation='tanh', weights=[Tan_Wr_init_weight, Tan_Wr_init_bias]), name='Tan_Wr1', input='r1')
+    model.add_node(Dense(k,W_regularizer=l2(opts.l2),activation='tanh', weights=[Tan_Wr_init_weight, Tan_Wr_init_bias]), name='Tan_Wr1', input='r1')
     Wr_init_weight = 2*(1/np.sqrt(k))*np.random.rand(k,k) - (1/np.sqrt(k))
     Wr_init_bias = 2*(1/np.sqrt(k))*np.random.rand(k,) - (1/np.sqrt(k))
-    model.add_node(Dense(k,W_regularizer=l2(0.01), weights=[Wr_init_weight, Wr_init_bias]), name='Wr1', input='r1')
+    model.add_node(Dense(k,W_regularizer=l2(opts.l2), weights=[Wr_init_weight, Wr_init_bias]), name='Wr1', input='r1')
     model.add_node(RepeatVector(L), name='Wr1_cross_e', input='Wr1')
 
     # GET R2, R3, .. R_N
@@ -114,13 +120,13 @@ def build_model(opts, verbose=False):
         model.add_node(Lambda(f, output_shape=(k,)), name='Wh_lp'+str(i), input='Wh_hypo')
         model.add_node(RepeatVector(L), name='Wh_lp'+str(i)+'_cross_e', input='Wh_lp'+str(i))
         model.add_node(Activation('tanh'), name='M'+str(i), inputs=['Wh_lp'+str(i)+'_cross_e', 'WY', 'Wr'+str(i-1)+'_cross_e'], merge_mode='sum')
-        model.add_node(TimeDistributedDense(1,activation='softmax'), name='alpha'+str(i), input='M'+str(i))
+        model.add_node(TimeDistributedDense(1,activation='softmax',weights=[Distributed_Dense_init_weight,Distributed_Dense_init_bias]), name='alpha'+str(i), input='M'+str(i))
         model.add_node(Lambda(get_R, output_shape=(k,1)), name='_r'+str(i), inputs=['Y','alpha'+str(i)], merge_mode='join')
         model.add_node(Reshape((k,)),name='*r'+str(i), input='_r'+str(i))
         model.add_node(Layer(), merge_mode='sum', inputs=['*r'+str(i),'Tan_Wr'+str(i-1)], name='r'+str(i))
         if i != (N-L):
-            model.add_node(Dense(k,W_regularizer=l2(0.01),activation='tanh', weights=[Tan_Wr_init_weight, Tan_Wr_init_bias]), name='Tan_Wr'+str(i), input='r'+str(i))
-            model.add_node(Dense(k,W_regularizer=l2(0.01), weights=[Wr_init_weight, Wr_init_bias]), name='Wr'+str(i), input='r'+str(i))
+            model.add_node(Dense(k,W_regularizer=l2(opts.l2),activation='tanh', weights=[Tan_Wr_init_weight, Tan_Wr_init_bias]), name='Tan_Wr'+str(i), input='r'+str(i))
+            model.add_node(Dense(k,W_regularizer=l2(opts.l2), weights=[Wr_init_weight, Wr_init_bias]), name='Wr'+str(i), input='r'+str(i))
             model.add_node(RepeatVector(L), name='Wr'+str(i)+'_cross_e', input='Wr'+str(i))
 
 #    model.add_node(Activation('tanh'), name='M', inputs=['Wh_n_cross_e', 'WY'], merge_mode='sum')
@@ -129,8 +135,8 @@ def build_model(opts, verbose=False):
 #    model.add_node(Reshape((k,)),name='r', input='_r')
 #    model.add_node(Dense(k,W_regularizer=l2(0.01)), name='Wr', input='r')
 
-    model.add_node(Dense(k,W_regularizer=l2(0.01)), name='Wr', input='r'+str(N-L)) ##### ADDED
-    model.add_node(Dense(k,W_regularizer=l2(0.01)), name='Wh', input='h_n')
+    model.add_node(Dense(k,W_regularizer=l2(opts.l2)), name='Wr', input='r'+str(N-L)) ##### ADDED
+    model.add_node(Dense(k,W_regularizer=l2(opts.l2)), name='Wh', input='h_n')
     model.add_node(Activation('tanh'), name='h_star', inputs=['Wr', 'Wh'], merge_mode='sum')
 
     model.add_node(Dense(3, activation='softmax'), name='out', input='h_star')
@@ -234,18 +240,19 @@ if __name__ == "__main__":
     setattr(K,'params',params)
 
     config_str = getConfig(options)
-    MODEL_ARCH = "/home/ee/btech/ee1130798/Code/Model/arch_att" + config_str + ".yaml"
-    MODEL_WGHT = "/home/ee/btech/ee1130798/Code/Model/weights_att" + config_str + ".weights"
+    MODEL_ARCH = "/home/ee/btech/ee1130798/Code/Models/arch_att" + config_str + ".yaml"
+    MODEL_WGHT = "/home/ee/btech/ee1130798/Code/Models/weights_att" + config_str + ".weights"
 #    MODEL_ARCH = "/Users/Shantanu/Documents/College/SemVI/COL772/Project/Code/Models/GloveEmbd/arch_att" + config_str + ".yaml"
 #    MODEL_WGHT = "/Users/Shantanu/Documents/College/SemVI/COL772/Project/Code/Models/GloveEmbd/weights_att" + config_str + ".weights"
    
-    MAXLEN=options.xmaxlen
-    X_train = pad_sequences(X_train, maxlen=MAXLEN,value=vocab["unk"],padding='pre')
-    X_dev = pad_sequences(X_dev, maxlen=MAXLEN,value=vocab["unk"],padding='pre')
-    X_test = pad_sequences(X_test, maxlen=MAXLEN,value=vocab["unk"],padding='pre')
-    Y_train = pad_sequences(Y_train, maxlen=MAXLEN,value=vocab["unk"],padding='post')
-    Y_dev = pad_sequences(Y_dev, maxlen=MAXLEN,value=vocab["unk"],padding='post')
-    Y_test = pad_sequences(Y_test, maxlen=MAXLEN,value=vocab["unk"],padding='post')
+    XMAXLEN=options.xmaxlen
+    YMAXLEN=options.ymaxlen
+    X_train = pad_sequences(X_train, maxlen=XMAXLEN,value=vocab["unk"],padding='pre')
+    X_dev = pad_sequences(X_dev, maxlen=XMAXLEN,value=vocab["unk"],padding='pre')
+    X_test = pad_sequences(X_test, maxlen=XMAXLEN,value=vocab["unk"],padding='pre')
+    Y_train = pad_sequences(Y_train, maxlen=YMAXLEN,value=vocab["unk"],padding='post')
+    Y_dev = pad_sequences(Y_dev, maxlen=YMAXLEN,value=vocab["unk"],padding='post')
+    Y_test = pad_sequences(Y_test, maxlen=YMAXLEN,value=vocab["unk"],padding='post')
    
     net_train=concat_in_out(X_train,Y_train,vocab)
     net_dev=concat_in_out(X_dev,Y_dev,vocab)
@@ -329,6 +336,7 @@ if __name__ == "__main__":
         print "Training Accuracy: ", train_acc
         print "Dev Accuracy: ", dev_acc
         print "Testing Accuracy: ", test_acc
-        test_acc=compute_acc(net_test, Z_test, vocab, model, options, "/home/ee/btech/ee1130798/Code/Test_Predictions.txt")
+        path = "/home/ee/btech/ee1130798/Code/Test_Predictions"+ config_str +".txt"
+        test_acc=compute_acc(net_test, Z_test, vocab, model, options, path)
 
-#        save_model(model,MODEL_WGHT,MODEL_ARCH)
+        save_model(model,MODEL_WGHT,MODEL_ARCH)
